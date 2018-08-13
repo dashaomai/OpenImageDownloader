1a4cf402e60e49134978ec98b45c34a69967c596
Buddy John <dashaomai@qq.com>
2018-08-13 12:43:45 +0800

    修改成为多协程下载版本

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"os"
 	"io"
 	"fmt"
@@ -10,28 +11,51 @@ import (
 	"net/http"
 )
 
+type DownloadTask struct {
+	url string
+	size int64
+	code string
+}
+
 func main() {
 	fmt.Println("========= Downloader Started! =========")
 
 	files := os.Args[1:]
 	if len(files) == 0 {
 	} else {
+		var n = flag.Int("n", 8, "number of threads")
+		flag.Parse()
+
+		var threadNumber = *n
+
 		for _, file := range files {
 			f, err := os.Open(file)
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "ImageDownloader: %v\n", err)
+				// 文件不存在，可能是其它控制参数
+				// fmt.Fprintf(os.Stderr, "ImageDownloader: %v\n", err)
 				continue
 			}
 
-			scanLines(f)
+			scanLines(f, threadNumber)
 			f.Close()
 		}
 	}
 }
 
-func scanLines(f *os.File) {
+func scanLines(f *os.File, threadNumber int) {
 	input := bufio.NewScanner(f)
-	ch := make(chan string)
+
+	fmt.Printf("%d thread(s) should created.\n", threadNumber)
+
+	// 创建指定数量的 channel 用于和一一对应的协程进行通讯
+	chs := make([] chan DownloadTask, threadNumber, threadNumber)
+	for i := 0; i<threadNumber; i++ {
+		ch := make(chan DownloadTask)
+		chs[i] = ch
+		go download(ch)
+	}
+
+	var pos = 0
 
 	for input.Scan() {
 		line := input.Text()
@@ -46,13 +70,41 @@ func scanLines(f *os.File) {
 				continue
 			}
 
-			go download(url, size, code, ch)
-			fmt.Println(<-ch)
+			// 挑选一个空闲协程，提交下载任务
+			for {
+				// pos 步进
+				if pos < threadNumber - 1 {
+					pos += 1
+				} else {
+					pos = 0
+				}
+
+				// fmt.Printf("pos: %d, len: %d, cap: %d\n", pos, len(chs[pos]), cap(chs[pos]))
+
+				if len(chs[pos]) == 0 {
+					// fmt.Printf("Ready to active #%d thread.\n", pos)
+					chs[pos] <- DownloadTask{url, size, code}
+
+					break
+				}
+			}
 		}
 	}
 }
 
-func download(url string, size int64, _ string, ch chan<- string) {
+// 分配任务的进程
+func download(ch <-chan DownloadTask) {
+	for task := range ch {
+		// fmt.Printf("Process: %v\n", task)
+		download2(task)
+		// fmt.Printf("Processed: %v\n", task)
+	}
+}
+
+func download2(task DownloadTask) {
+	url := task.url
+	size := task.size
+
 	// 检查文件是否已经存在
 	splited := strings.Split(url, "/")
 	filename := "downloads/" + splited[len(splited) - 1]
@@ -60,7 +112,7 @@ func download(url string, size int64, _ string, ch chan<- string) {
 	if err == nil {
 		stat, err := f0.Stat()
 		if err == nil && stat.Size() == size {
-			ch <- fmt.Sprintf("%s already downloaded!", filename)
+			fmt.Printf("%s already downloaded!\n", filename)
 			return
 		}
 	}
@@ -70,28 +122,31 @@ func download(url string, size int64, _ string, ch chan<- string) {
 		defer resp.Body.Close()
 	}
 	if err != nil {
-		ch <- fmt.Sprint(err)
+		fmt.Printf("%v\n", err)
+		return
+	}
+
+	nbytes := resp.ContentLength
+	if size != nbytes {
+		fmt.Printf("%s size incorrect: %d -> %d\n", url, size, nbytes)
 		return
 	}
 
 	f, err := os.Create(filename)
-	defer f.Close()
+	if (f != nil) {
+		defer f.Close()
+	}
+
 	if err != nil {
-		ch <- fmt.Sprint(err)
+		fmt.Printf("%v\n", err)
 		return
 	}
 
-	nbytes, err := io.Copy(f, resp.Body)
-	if err != nil {
-		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+	_, err2 := io.Copy(f, resp.Body)
+	if err2 != nil {
+		fmt.Printf("while reading %s: %v\n", url, err2)
 		return
 	}
 
-	if size != nbytes {
-		ch <- fmt.Sprintf("%s size incorrect: %d -> %d", url, size, nbytes)
-		os.Remove(filename)
-		return
-	}
-
-	ch <- fmt.Sprintf("%s download to %s successful!", url, filename)
+	fmt.Printf("%s download successful!\n", url)
 }
